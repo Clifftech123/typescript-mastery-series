4021647389c21b8413d40a3f4ccf5a3a63d4b4a3
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-11-25 00:45:34 +0000

    Startin the part one

---
+++ src/part-1.ts
@@ -0,0 +1,15 @@
+// decalring a variable in typescript
+
+let a: number = 10;
+let b: string = "Hello World";
+let c: boolean = true;
+let d: any = "Hello World";
+let e: any = 10;
+let f: any = true;a
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
