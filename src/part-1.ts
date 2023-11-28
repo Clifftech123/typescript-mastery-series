2a2c3145f075bba0aa7b8ffd3271594b906f6d61
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-11-28 13:09:58 +0000

    Adding of the codes :

--- src/part-1.ts
+++ src/part-1.ts
@@ -1,15 +1,30 @@
-// decalring a variable in typescript
+// The // greet
 
-let a: number = 10;
-let b: string = "Hello World";
-let c: boolean = true;
-let d: any = "Hello World";
-let e: any = 10;
-let f: any = true;a
+// function greet1(name: string): void {
+//   console.log(`Hello, ${name}!`);
+// }
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
+// greet1("TypeScript"); // Hello, TypeScript!
+
+
+class Person {
+  constructor(public name: string) {}
+
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+}
+
+// Using the class
+let person1 = new Person("TypeScript");
+person1.greet(); // Hello, TypeScript!
+
+let person2 = new Person("John Doe");
+person2.greet(); // Hello, John Doe
+
+
+
+let age: number = 25;
+age = 20 + 5;  // 25
+console.log(age);
+
